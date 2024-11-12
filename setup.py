33fe6352ef7aa9265ee0c43de26222cc92c459c8
zackees <z@zackees.com>
2024-11-11 22:33:02 -0800

    update

---
+++ setup.py
@@ -0,0 +1,23 @@
+"""
+Setup file.
+"""
+
+import os
+import re
+
+from setuptools import setup
+
+URL = "https://github.com/zackees/semi-secret"
+KEYWORDS = "Semi Secret Key Value Storage, provides more security than plain text json file"
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+
+
+if __name__ == "__main__":
+    setup(
+        maintainer="Zachary Vorhies",
+        keywords=KEYWORDS,
+        url=URL,
+        package_data={"": ["assets/example.txt"]},
+        include_package_data=True)
+
